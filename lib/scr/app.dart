c778d90ca60a5ce2d72fa90cad1903fc293cbd6c
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2024-10-08 23:58:45 -0400

    Demo Build

--- lib/scr/app.dart
+++ lib/scr/app.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:flutter_blog_app/scr/blog_list.dart';
 import 'package:google_fonts/google_fonts.dart';
+=======
+import 'package:flutter_blog_app/scr/home_feature/homePage.dart';
+>>>>>>> e60087a (Demo Build)
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return MaterialApp(
       theme: ThemeData(textTheme: GoogleFonts.interTightTextTheme()),
       home: const BlogList(),
+=======
+    return const MaterialApp(
+      home: HomePage(),
+>>>>>>> e60087a (Demo Build)
     );
   }
 }
---
+++ lib/scr/home_feature/homePage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/scr/blog_feature/blogPage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    BlogPage(),
+    BlogPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
