2f07ec91f68f15fa6e3ae8e002aa7e19641c52ff
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2024-10-09 00:19:55 -0400

    AddPostPage

--- lib/scr/blog_feature/blogPage.dart
+++ lib/scr/blog_feature/blogPage.dart
@@ -5,6 +5,27 @@ class BlogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blogs',
+          style: TextStyle(fontWeight: FontWeight.bold,
+          fontSize: 28),
+        ),
+
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.favorite_border, size: 26,)
+          ),
+        ],
+      ),
+
+      body: ListView(
+        children: const <Widget>[
+
+        ],
+      ),
+    );
   }
 }
