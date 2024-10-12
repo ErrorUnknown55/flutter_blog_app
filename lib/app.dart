54768209d1ec4ed0b093e3cc94977cf8ccefc74e
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2024-10-12 01:13:01 -0400

    Added Feed

--- lib/app.dart
+++ lib/app.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
+    return const MaterialApp(
       //theme: ThemeData(textTheme: GoogleFonts.interTightTextTheme()),
       home: HomePage()
     );
